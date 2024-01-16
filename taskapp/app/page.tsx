a65d521937b4f07d29408683a851d05c08571d7b
Haripriya <haripriya14022003@gmail.com>
2024-01-16 14:51:38 +0530

    Added the navbar and header

--- taskapp/app/page.tsx
+++ taskapp/app/page.tsx
@@ -1,3 +1,11 @@
+import AddTask from './components/Navbar';
+import TodoList from './components/TopicList';
 export default function Home(){
-  return<main></main>
+  return(
+  <main className="max-w-4xl mx-auto">
+   <div className="text-center my-5 flex-col gap-4">
+  
+   </div>
+  </main>
+  );
 }
